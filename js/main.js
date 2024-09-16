89d30328afe275f9d978a29ff2e981a63833eac6
Alex V.B. <136813525+wi-p@users.noreply.github.com>
2024-09-16 09:31:34 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function changeToPT(){
+    document.getElementById("p1").innerHTML = `
+        Olá, meu nome é Alex, eu sou
+        um programador fascinado por computadores, linguagens de programação, hardwares, e tecnologias. Eu gosto muito
+        destes assuntos. Eu criei este simples website para mostrar às pessoas os meus projetos.`;
+    document.getElementById("p2").innerHTML = `
+        Atualmente, eu estou estudando HTML, CSS, JS, Python, Django, Banco de dados, e aplicativos móveis e para
+        computadores usando a linguagem python. Ademais, eu trabalhei com Arduino e componentes eletrônicos, construindo hardwares e firmwares usados
+        em projetos acadêmicos.`;
+    document.getElementById("p3").innerHTML = `
+    <strong class="advice">Atenção!!</strong>  Você é livre para usar meus projetos, mas por favor, eu espero que você faça menção da minha pessoa caso
+        esteja usando esses projetos em um ambiente não privado. Abaixo, você encontra uma lista com alguns dos meus projetos
+        open-source e os links para os diretórios onde você pode encontrá-los, fazer o download e usá-los totalmente gratuito.`;
+    document.getElementById("p4").innerHTML = "Um website para postar e baixar músicas";
+    document.getElementById("p5").innerHTML = "Um website para postar e ler publicações";
+    document.getElementById("p6").innerHTML = "Um website onde clientes podem saber a rota";
+    document.getElementById("p7").innerHTML = "Um aplicativo para gerenciar o mercado";
+}
+
+function changeToEN(){
+    document.getElementById("p1").innerHTML = `
+        Hello, my name is Alex. I'm a programmer fascinated by computers,
+        programming languages, hardwares, and technologies, I really like these things. I've created this simple
+        website to show the others my projects.`;
+    document.getElementById("p2").innerHTML = `
+        Nowdays, I'm studying HTML, CSS, JS, Python, Django, Data Base, mobile and desktop applications using python. 
+        Further more, I've worked with Arduino and electronic components, building hardwares and firmwares used in 
+        school projects.`;
+    document.getElementById("p3").innerHTML = `
+    <strong class="advice">Attention!!</strong>  You are free to use my projects, but please, I hope you make a 
+        mention of myself if you're using these projects in a non-private environment. Bellow, you can find a list
+        with some of my open-source projects and the links that take your to the directories where you can download 
+        and use them for free`;
+    document.getElementById("p4").innerHTML = "Website used to post and download musics";
+    document.getElementById("p5").innerHTML = "Website used to post and read publications";
+    document.getElementById("p6").innerHTML = "A website where clients can see the rote";
+    document.getElementById("p7").innerHTML = "An App used by owner to manage";
+}
